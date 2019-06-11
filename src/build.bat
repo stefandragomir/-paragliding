a61617b0985ceb49b986fa2b7f31ffcab654c9f2
stefan.dragomir <stefan.dragomir@yazaki-europe.com>
2019-06-11 14:09:18 +0300

    Added release 0.0.1

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,1 @@
-d:\toolbox\python3\Scripts\pyinstaller.exe -y --debug=all --windowed --icon=parag.ico parag\parag.py
+c:\toolbox\python3\Scripts\pyinstaller.exe -y --debug=all --windowed --icon=parag.ico parag\parag.py
